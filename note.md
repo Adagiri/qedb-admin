6b491193914b162e3315735b9ba256c8ff580b1b
adagiri <ibrahimridwan47@gmail.com>
2021-11-11 12:32:56 +0100

    lll

---
+++ note.md
@@ -0,0 +1,2 @@
+we need to import necessary components\
+we need to include a dataProvider prop and pass in a provider to our Admin component
--- src/components/admin/ReactAdmin.js
+++ src/components/admin/ReactAdmin.js
@@ -1,9 +1,13 @@
-// in src/App.js
 import * as React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./users";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
