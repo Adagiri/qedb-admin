334fc1d7637e852b069b0b587bf6285a36956e6d
adagiri <ibrahimridwan47@gmail.com>
2021-11-12 23:16:20 +0100

    progress

--- src/components/admin/users.js
+++ src/components/admin/users.js
@@ -1,16 +1,17 @@
 import { List, Datagrid, TextField, EmailField } from "react-admin";
+import CustomUrlField from "./CustomUrlField";
 
 export const UserList = (props) => {
   return (
     <List {...props}>
       <Datagrid rowClick="edit">
-        <TextField source="id" />
+        {/* <TextField source="id" /> */}
         <TextField source="name" />
-        <TextField source="username" />
+        {/* <TextField source="username" /> */}
         <EmailField source="email" />
-        <TextField source="address.street" />
+        {/* <TextField source="address.street" /> */}
         <TextField source="phone" />
-        <TextField source="website" />
+        <CustomUrlField source="website" />
         <TextField source="company.name" />
       </Datagrid>
     </List>
